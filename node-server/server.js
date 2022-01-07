6ade0c17ccced580ba14cab7842b1ccf1c189fd6
EstebanHernandez1 <estebanahr@gmail.com>
2022-01-07 10:26:45 -0600

    add files

---
+++ node-server/server.js
@@ -0,0 +1,36 @@
+const express = require('express'); // imported express module or top level class of express
+const port = 9090;
+const app = express(); //by invoking top level class we are initilizing the application
+const cors = require("cors");
+const router = require('./route/router');
+const adminRouter = require('./route/adminRouter');
+const userRouter = require('./route/userRouter');
+const productRouter = require('./route/productRouter');
+const cartRouter = require('./route/cartRouter');
+
+const adminApp = express();
+const userApp = express();
+const productApp = express();
+const cartApp = express();
+
+app.use(cors());//cors - middleware is passed in express application to api's being public at global level
+app.use('/static', express.static('public')); // serve static files like images css using static middle ware
+
+app.use(express.json({limit:'2mb', extended:false})); //json middle-ware for setting request content type
+
+app.use('/admin', adminApp);
+adminApp.use('/', adminRouter);
+
+app.use('/user', userApp);
+userApp.use('/', userRouter);
+
+app.use('/product', productApp);
+productApp.use('/', productRouter);
+
+app.use('/cart', cartApp);
+cartApp.use('/', cartRouter);
+
+app.use('/', router); // all the requests coming to express app are routed to router.js
+
+console.log(`we are listening on port ${port} with url http://localhost:9090`)
+app.listen(port)
